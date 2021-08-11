12c057b0a98a4bbb5f9bd065ec4c7ba13969aa71
Coniper <Coniper@outlook.com>
2021-08-11 09:26:56 +0800

    更新 12_test.c

--- 2day/12_test.c
+++ 2day/12_test.c
@@ -12,42 +12,46 @@ int main(int argc, char *argv[])
 {
     if(2 >= argc)
     {
-        printf("need more argmenus\n");
+        printf("Uage <src filename> <des filename>\n");
         return -1;
     }
-    char buf[SIZE] = {0};
     
-    FILE* fp1 = fopen(argv[1], "r");
-    if(NULL == fp1)
+    FILE* sfp = fopen(argv[1], "r");
+    if(NULL == sfp)
     {
-        perror("fopen_1");
+        perror("fopen_sfp");
         return -1;
     }
 
-    FILE* fp2 = fopen(argv[2], "w");
-    if(NULL == fp2)
+    FILE* dfp = fopen(argv[2], "w");
+    if(NULL == dfp)
     {
-        perror("fopen_2");
+        perror("fopen_dfp");
         return -1;
     }
 
-    size_t temp = 0;
+    size_t count = 0;
+    char buf[SIZE] = {0};
 
+/*
     for(;;)
     {
-        fread(buf, SIZE, 1, fp1);
-        temp = fwrite(buf, SIZE, 1, fp2);
+        fread(buf, 1, SIZE, sfp);
+        temp = fwrite(buf, 1, SIZE, dfp);
         
 
-        if(feof(fp1) == temp)
+        if(feof(sfp) == count)
         {
             printf("copy is complete\n");
             break;
         }
     }
+*/
+    while( (counte = fread(buf, 1, SIZE, sfp)) > 0 )
+        fwrite(buf, 1, SIZE, dfp)
 
-    fclose(fp1);
-    fclose(fp2);
+    fclose(sfp);
+    fclose(dfp);
 
     return 0;
 }
